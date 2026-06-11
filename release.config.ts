a42a6dde903d7a82d72adf9187db7f00f28cbec6
ABiteofPi <payabiteofpi@gmail.com>
2026-06-11 18:03:53 +0200

    added pipeline

---
+++ release.config.ts
@@ -0,0 +1,15 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]',
+    }],
+    '@semantic-release/github',
+  ],
+};
+
+export default config;
