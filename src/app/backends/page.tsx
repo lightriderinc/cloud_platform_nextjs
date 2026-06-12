f6ad952943231fe6c7d1a662f8f9d6af8deb73e7
Dan-Delgado-Ayala <dayala@lightriderinc.com>
2026-06-12 11:33:46 -0400

    feat: add placeholder page component and created placeholders  for all sidebar routes

---
+++ src/app/backends/page.tsx
@@ -0,0 +1,10 @@
+import PagePlaceholder from "@/components/PagePlaceholder";
+
+export default function BackendsPage() {
+  return (
+    <PagePlaceholder
+      title="Backends"
+      description="QPUs and simulators available to run your circuits."
+    />
+  );
+}
---
+++ src/app/jobs/page.tsx
@@ -0,0 +1,10 @@
+import PagePlaceholder from "@/components/PagePlaceholder";
+
+export default function JobsPage() {
+  return (
+    <PagePlaceholder
+      title="Jobs"
+      description="Track and manage your submitted jobs."
+    />
+  );
+}
