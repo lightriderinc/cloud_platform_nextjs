3d6072324f1b22649883acc5b6038c0e3f16b6ce
ABiteofPi <payabiteofpi@gmail.com>
2026-06-15 16:28:48 +0200

    fix: added brand gradient class

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div className="flex mt-5 flex-col items-center justify-center gap-4">
       <div className="flex flex-col items-center justify-center gap-1">
-        <h1 style={{ fontSize: "10rem" }} className="color-brand-primary">404</h1>
+        <h1 style={{ fontSize: "10rem" }} className="text-brand-gradient">404</h1>
         <h1>Page not found</h1>
       </div>
 
