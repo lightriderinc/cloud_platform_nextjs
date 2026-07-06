672701aa2d5c3b7cd15ec84f3a68b62b86033b43
ABiteofPi <payabiteofpi@gmail.com>
2026-07-06 18:13:35 +0200

    fix: updated md packages for table rendering

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
-import createMDX from '@next/mdx'
-import type { NextConfig } from 'next'
+import createMDX from '@next/mdx';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
 const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: ['remark-gfm'],
+    rehypePlugins: [],
+  },
 })
 
 export default withMDX(nextConfig)
