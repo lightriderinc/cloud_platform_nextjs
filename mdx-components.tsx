6dcf11a6fa68dcb20c29cc4a87b9805f191ac942
ABiteofPi <payabiteofpi@gmail.com>
2026-07-06 17:34:25 +0200

    fix: privacy policy + ToS pages added

---
+++ mdx-components.tsx
@@ -0,0 +1,11 @@
+import type { MDXComponents } from "mdx/types";
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    h1: (props) => <h1 className="text-2xl font-semibold text-gray-700" {...props} />,
+    h2: (props) => <h2 className="mb-3 text-xl font-bold text-gray-600" {...props} />,
+    a: (props) => <a className="text-blue-600 hover:text-blue-500" {...props} />,
+
+    ...components,
+  };
+}
