0579ea453a20cb11dffaadf4a5c5e6679f8dafbd
Dan-Delgado-Ayala <dayala@lightriderinc.com>
2026-08-13 15:55:06 -0400

    chore: undid process.env.VARIABLE rename

--- src/app/logto.ts
+++ src/app/logto.ts
@@ -2,9 +2,9 @@ export const logtoConfig = {
   managementEndpoint: 'https://4e7d0k.logto.app/',
   endpoint: 'https://4e7d0k.logto.app/',
   appId: 'vqq5tqptgjud45vt23yfs',
-  appSecret: process.env.DEV_LOGTO_APP_SECRET ,
-  baseUrl: process.env.NEXT_DEV_BASE_URL || 'http://localhost:3000', // Change to your own base URL
-  cookieSecret: process.env.DEV_LOGTO_COOKIE_SECRET as string, // Auto-generated 32 digit secret
+  appSecret: process.env.LOGTO_APP_SECRET ,
+  baseUrl: process.env.NEXT_BASE_URL || 'http://localhost:3000', // Change to your own base URL
+  cookieSecret: process.env.LOGTO_COOKIE_SECRET as string, // Auto-generated 32 digit secret
   cookieSecure: process.env.NODE_ENV === 'production',
   proRoleID: "d4px0fafm78qfb574wgfm",
 };
