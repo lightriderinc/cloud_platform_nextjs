7a01048d5626c9893457ddd92572ec97e0e113cf
Dan-Delgado-Ayala <dayala@lightriderinc.com>
2026-08-13 14:23:14 -0400

    fix: added fallback to base URL to avoid typescript fail

--- src/app/logto.ts
+++ src/app/logto.ts
@@ -3,7 +3,7 @@ export const logtoConfig = {
   endpoint: 'https://4e7d0k.logto.app/',
   appId: 'vqq5tqptgjud45vt23yfs',
   appSecret: 'ehaOI5tObMldStzqk3o3R8YgUqTBOrNj',
-  baseUrl: process.env.NEXT_DEV_BASE_URL, // Change to your own base URL
+  baseUrl: process.env.NEXT_DEV_BASE_URL || 'http://localhost:3000', // Change to your own base URL
   cookieSecret: 'TAliBkZPfmuENQPbrgwxzb17VDhrBj4T', // Auto-generated 32 digit secret
   cookieSecure: process.env.NODE_ENV === 'production',
   proRoleID: "d4px0fafm78qfb574wgfm",
