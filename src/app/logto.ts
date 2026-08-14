4712d5fa1bc06c3e21959d570ead9168452f3dc3
Dan-Delgado-Ayala <dayala@lightriderinc.com>
2026-08-14 09:38:26 -0400

    Fix: I missed a variable in last change

--- src/app/logto.ts
+++ src/app/logto.ts
@@ -1,10 +1,10 @@
 export const logtoConfig = {
   managementEndpoint: process.env.LOGTO_MANAGEMENT_ENDPOINT as string, // Change to your own management endpoint
   endpoint: process.env.LOGTO_ENDPOINT as string, // Change to your own endpoint
-  appId: process.env.LOGTO_APP_ID, // Change to your own app ID
+  appId: process.env.LOGTO_APP_ID as string, // Change to your own app ID
   appSecret: process.env.LOGTO_APP_SECRET as string, // Change to your own app secret
   baseUrl: process.env.NEXT_BASE_URL || 'http://localhost:3000', // Change to your own base URL
   cookieSecret: process.env.LOGTO_COOKIE_SECRET as string, // Auto-generated 32 digit secret
   cookieSecure: process.env.NODE_ENV === 'production',
-  proRoleID: process.env.LOGTO_PRO_ROLE_ID, // Change to your own role ID
+  proRoleID: process.env.LOGTO_PRO_ROLE_ID as string, // Change to your own role ID
 };
