8b9eacdf2120af9c8c65729794a9a49d02484c87
Dan-Delgado-Ayala <dayala@lightriderinc.com>
2026-08-14 09:32:32 -0400

    fix: Changed process.env call to as string

--- src/app/logto.ts
+++ src/app/logto.ts
@@ -1,6 +1,6 @@
 export const logtoConfig = {
-  managementEndpoint: process.env.LOGTO_MANAGEMENT_ENDPOINT, // Change to your own management endpoint
-  endpoint: process.env.LOGTO_ENDPOINT, // Change to your own endpoint
+  managementEndpoint: process.env.LOGTO_MANAGEMENT_ENDPOINT as string, // Change to your own management endpoint
+  endpoint: process.env.LOGTO_ENDPOINT as string, // Change to your own endpoint
   appId: process.env.LOGTO_APP_ID, // Change to your own app ID
   appSecret: process.env.LOGTO_APP_SECRET as string, // Change to your own app secret
   baseUrl: process.env.NEXT_BASE_URL || 'http://localhost:3000', // Change to your own base URL
