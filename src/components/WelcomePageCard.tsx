314ac65e1de97951310d41e16683ce33a9c6abf2
ABiteofPi <payabiteofpi@gmail.com>
2026-09-25 15:53:57 +0200

    fix: welcome page cards updated

--- src/components/WelcomePageCard.tsx
+++ src/components/WelcomePageCard.tsx
@@ -24,8 +24,8 @@ export default function WelcomePageCard({
       target={external ? "_blank" : undefined}
       rel={external ? "noopener noreferrer" : undefined}
     >
-      <div className="flex flex-col h-full w-full bg-gray-100 p-5 border border-gray-100 default-radius card-hover-primary gap-3">
-        <Icon className="text-5xl text-gray-200" />
+      <div className="flex flex-col h-full w-full bg-gray-100 p-5 border border-gray-100 default-radius card-hover-primary gap-3 group">
+        <Icon className="text-5xl text-gray-200 group-hover:text-[var(--brand-tertiary)] transition duration-150" />
         <h3 className="flex items-center gap-1 text-md font-medium">
           {title} {external && <MdArrowOutward />}
         </h3>
